9adb25bb97268f1b10491e25d01196e46021b47d
Michael Lan <michaellan202@gmail.com>
2022-12-15 17:53:45 -0800

    nicer

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import Brick
 import Brick.Widgets.Edit qualified as E
+import Control.Applicative
 import Control.Lens hiding (zoom)
 import Control.Lens.TH (makeLenses)
 import Control.Monad
@@ -18,17 +20,17 @@ import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Vector (Vector, (!))
 import Data.Vector qualified as V
-import Graphics.Vty
+import Graphics.Vty hiding ((<|>))
 import Network.HTTP.Req
 import System.Exit
 import System.Process (rawSystem)
 
 data AppState = AppState
-  { _queryEditor :: E.Editor Text (),
-    _entries :: Vector Text,
-    _entrySelectPos :: Maybe Int,
-    _produce :: Text -> IO (Vector Text),
-    _confirm :: Text -> IO ()
+  { _queryEditor :: E.Editor Text ()
+  , _entries :: Vector Text
+  , _entrySelectPos :: Maybe Int
+  , _produce :: Text -> IO (Vector Text)
+  , _confirm :: Text -> IO ()
   }
 
 makeLenses ''AppState
@@ -41,7 +43,9 @@ handleEvent (VtyEvent (Ctrl k)) | k `elem` ['n', 'p'] = do
   st <- get
   let numEntries = length (st ^. entries)
   when (numEntries == 0) continueWithoutRedraw
-  let n' = st ^. entrySelectPos <&> \n -> max (n - 1) 0
+  let n'
+        | k == 'n' = min numEntries . (+ 1) <$> (st ^. entrySelectPos) <|> pure 0
+        | otherwise = st ^. entrySelectPos <&> \n -> max (n - 1) 0
   put $ st & entrySelectPos .~ n'
 handleEvent (VtyEvent (EvKey KEnter [])) = do
   st <- get
@@ -80,7 +84,7 @@ searchResults idx xs
     Just i -> V.foldr1 (<=>) (V.map txt xs & ix i %~ withAttr (attrName "selected"))
 
 dict :: IO (Vector Text)
-dict = V.fromList <$> (take 800 <$> (T.lines <$> T.readFile "/usr/share/dict/words"))
+dict = V.fromList . take 800 . T.lines <$> T.readFile "/usr/share/dict/words"
 
 dictSource :: Text -> IO (Vector Text)
 dictSource q = do
@@ -89,22 +93,19 @@ dictSource q = do
 googleSource :: Text -> IO (Vector Text)
 googleSource "" = pure $ pure "nothing to see here"
 googleSource q = do
-  r <- runReq defaultHttpConfig $ do
-    req
-      GET
-      (http "google.com" /: "complete" /: "search")
-      NoReqBody
-      jsonResponse
-      (("client" =: ("firefox" :: Text)) <> ("q" =: q))
+  r <- runReq defaultHttpConfig $ req GET url NoReqBody jsonResponse params
   case responseBody r of
-    _ : t@(Array xs) : _ -> pure $ V.fromList [x | String x <- V.toList xs]
+    _ : Array xs : _ -> pure $ V.fromList [x | String x <- V.toList xs]
     _ -> pure V.empty
+ where
+  url = http "google.com" /: "complete" /: "search"
+  params = ("client" =: ("firefox" :: Text)) <> ("q" =: q)
 
 drawUI (AppState q e s _ _) = [E.renderEditor (txt . T.unlines) True q <=> searchResults s e]
 
 attrs =
-  [ (attrName "selected", fg blue),
-    (E.editAttr, bg black)
+  [ (attrName "selected", fg blue)
+  , (E.editAttr, bg black)
   ]
 
 openBrowser :: Text -> IO ()
@@ -113,11 +114,11 @@ openBrowser = void . rawSystem "open" . pure . T.unpack
 app :: App AppState () ()
 app =
   App
-    { appDraw = drawUI,
-      appChooseCursor = showFirstCursor,
-      appHandleEvent = handleEvent,
-      appStartEvent = pure (),
-      appAttrMap = const $ attrMap defAttr attrs
+    { appDraw = drawUI
+    , appChooseCursor = showFirstCursor
+    , appHandleEvent = handleEvent
+    , appStartEvent = pure ()
+    , appAttrMap = const $ attrMap defAttr attrs
     }
 
 setupApp :: (Text -> IO (Vector Text)) -> (Text -> IO ()) -> AppState
